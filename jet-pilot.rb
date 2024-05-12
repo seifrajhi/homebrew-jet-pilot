0f4dbb8256f583dd47013f99470d43a209773be7
saifeddine Rajhi <rajhiseif@gmail.com>
2024-05-12 09:03:16 +0200

    Create jet-pilot.rb

---
+++ jet-pilot.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class JetPilot < Formula
+  desc "JET Pilot is an open-source Kubernetes desktop client that focuses on less clutter, speed and good looks."
+  homepage "https://jet-pilot.app"
+  url "https://github.com/unxsist/jet-pilot/archive/refs/tags/v1.25.0.tar.gz"
+  sha256 "35b667256244e9bc5d0d4feafccbf84584845c0d10341283666594258020d212"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test jet-pilot`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
